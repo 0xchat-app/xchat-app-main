4595c230998020e6698a926e14cb06d8f58618a2
W <130329555+wcat7@users.noreply.github.com>
2025-06-30 18:17:21 +0800

    update pub_get.sh

--- ox_pub_get.sh
+++ ox_pub_get.sh
@@ -1,167 +1,83 @@
 #!/usr/bin/env bash
-#Main Project directory
-mainPath=$(pwd)
-mainProjectName=${mainPath##*/}
+# =============================================================================
+# ox_pub_get.sh
+# Utility script to checkout specific branches for the 0xChat Lite repo and its
+# sub-projects, then run `flutter pub get` for each package.
+#
+# Supported repositories & default branches:
+#   1. Main project           → main  (flag: -m <branch>)
+#   2. 0xchat-core            → upgrade/isar4 (fixed)
+#   3. nostr-dart             → main  (flag: -n <branch>)
+#   4. nostr-mls-package      → main  (flag: -l <branch>)
+# =============================================================================
 
-#Submodule directory
-baseFrameworkPath=${mainPath}/packages/base_framework
-businessModulesPath=${mainPath}/packages/business_modules
-oxchatCorePath=${mainPath}/packages/0xchat-core
-nostrDartPath=${mainPath}/packages/nostr-dart
-webrtcPath=${mainPath}/packages/flutter-webrtc
-cashuPath=${mainPath}/packages/cashu-dart
+set -euo pipefail
 
+main_path="$(pwd)"
+core_path="${main_path}/packages/0xchat-core"
+nostr_dart_path="${main_path}/packages/nostr-dart"
+nostr_mls_path="${main_path}/packages/nostr-mls-package"
 
-#Exception
-check(){
-  if [[ ! $? -eq 0 ]]
-  then
-    echo "\033[31merror: An error occurred when ${submoduleName##*/} module $1，please check the above error message\033[0m"
-    exit 1
-  fi
-}
-
-
-#checkout branch
-checkoutBranch(){
-    submoduleName=$1
-    echo "--------------------------- Module Name：${submoduleName##*/} ---------------------------"
-    if [[ ${submoduleName##*/} == ${mainProjectName} ]]
-    then
-        if [[ ! "$(ls -A ${baseFrameworkPath})" || ! "$(ls -A ${businessModulesPath})" ]]
-        then
-            git submodule init
-            check "git submodule update"
-        	git submodule update
-        fi
-    fi
-
-    cd $1
-
-    git fetch
-
-    if [[ ! -n $2 ]]
-    then
-	    git checkout main
-    else
-	    git checkout $2
-    fi
-
-    check "checkout branch"
-
-    git pull
-
-    check "git pull"
-
-    #git log -5
-}
-
-
-checkoutBranchByAll(){
-    checkoutBranch ${mainPath} $1
-    checkoutBranch ${baseFrameworkPath} $2
-    checkoutBranch ${businessModulesPath} $3
-    checkoutBranch ${oxchatCorePath}
-    checkoutBranch ${nostrDartPath}
-    checkoutBranch ${webrtcPath}
-    checkoutBranch ${cashuPath}
-}
-
-
-#Execute 'flutter pub get'
-executePubGet(){
-    cd $1
-    for file in $(ls "$1")
-    do
-      path=$1"/"${file}
-      if [[ -d ${path} ]]
-      then
-      	  echo "---------------------------$file---------------------------"
-          cd $path
-          flutter pub get
-#           flutter clean
-      fi
-    done
-}
-
+# Default branches
+main_branch="main"
+core_branch="upgrade/isar4"       # Fixed
+nostr_branch="main"
+mls_branch="main"
 
 usage() {
-    echo "Usage:"
-    echo "ox_pub_get.sh [-m Main Project Branch Name] [-b Base Framework Branch Name] [-s Business Modules Name]"
-    exit -1
+  cat <<EOF
+Usage: ./ox_pub_get.sh [options]
+Options:
+  -m <branch>   Branch name for main project          (default: main)
+  -n <branch>   Branch name for packages/nostr-dart   (default: main)
+  -l <branch>   Branch name for packages/nostr-mls-package (default: main)
+  -h            Show this help message
+EOF
+  exit 1
 }
 
+# ------------------------- Parse CLI arguments ------------------------------
+while getopts ':m:n:l:h' opt; do
+  case "$opt" in
+    m) main_branch="$OPTARG" ;;
+    n) nostr_branch="$OPTARG" ;;
+    l) mls_branch="$OPTARG" ;;
+    h) usage ;;
+    :) echo "Option -$OPTARG requires an argument."; exit 1 ;;
+    \?) echo "Invalid option: -$OPTARG"; usage ;;
+  esac
+done
 
-case $# in
-    0)
-       checkoutBranchByAll main main main
-    ;;
-    2)
-       if [[ $1 == '-m' || $1 == '-b' || $1 == '-s' ]]
-       then
-       	while getopts ':m:b:s:' OPT; do
-           case $OPT in
-               m)
-                   checkoutBranchByAll $OPTARG main main
-               ;;
-               b)
-                   checkoutBranchByAll main $OPTARG main
-               ;;
-               s)
-                   checkoutBranchByAll main main $OPTARG
-               ;;
-               ?)
-                   usage
-               ;;
-           esac
-       	done
-       else
-	usage
-       fi
-    ;;
-    4)
-        if [[ $1 = '-m' && $3 = '-b' ]]
-        then
-           checkoutBranchByAll $2 $4 main
-        elif [[ $1 = '-m' && $3 = '-s' ]]
-        then
-           checkoutBranchByAll $2 main $4
-        elif [[ $1 = '-b' && $3 = '-s' ]]
-        then
-           checkoutBranchByAll main $2 $4
-        else
-            usage
-        fi
-    ;;
-    6)
-        while getopts ':m:b:s:' OPT; do
-            case $OPT in
-                m)
-                    checkoutBranch ${mainPath} $OPTARG
-                ;;
-                b)
-                    checkoutBranch ${baseFrameworkPath} $OPTARG
-                ;;
-                s)
-                    checkoutBranch ${businessModulesPath} $OPTARG
-                ;;
-                ?)
-                    usage
-                ;;
-            esac
-        done
-    ;;
-    *)
-    usage
-    ;;
-esac
+# ------------------------- Helper functions ---------------------------------
+checkout_branch() {
+  local dir=$1
+  local branch=$2
+  echo "\n--- Checkout $(basename "$dir") => $branch ---"
+  if [[ ! -d "$dir/.git" ]]; then
+    echo "Directory $dir is not a git repo, skipping."
+    return
+  fi
+  git -C "$dir" fetch --all --tags
+  git -C "$dir" checkout "$branch"
+  git -C "$dir" pull --ff-only || true
+}
 
-executePubGet ${baseFrameworkPath}
+run_pub_get() {
+  local dir=$1
+  echo "Running flutter pub get in $dir"
+  (cd "$dir" && flutter pub get)
+}
 
-executePubGet ${businessModulesPath}
+# ------------------------- Checkout branches --------------------------------
+checkout_branch "$main_path" "$main_branch"
+checkout_branch "$core_path" "$core_branch"
+checkout_branch "$nostr_dart_path" "$nostr_branch"
+checkout_branch "$nostr_mls_path" "$mls_branch"
 
+# ------------------------- Run flutter pub get ------------------------------
+run_pub_get "$main_path"
+run_pub_get "$core_path"
+run_pub_get "$nostr_dart_path"
+run_pub_get "$nostr_mls_path"
 
-cd ${mainPath}
-
-flutter pub get
-#flutter clean
+echo "\n✔ All done."
